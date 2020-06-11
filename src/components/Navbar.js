f25a6381610fc6ee307a3d1927d3900572a01dd5
liyaSileshi <liyasileshi@gmail.com>
2020-06-11 00:04:05 -0700

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return(
+    <div className='btn-container'>
+      <button>Home</button>
+      <button>Game</button>
+      <button>About</button>
+    </div>
+    
+  )
+}
+
+export default Navbar;
